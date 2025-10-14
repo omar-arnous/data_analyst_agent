e36e6e1638245c55423ff61f7eadace7ab50d0d4
omar-arnous <omar99.arnous@gmail.com>
2025-10-14 08:52:37 +0300

    read sals statistics from csv file

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
-from tempfile import template
+import pandas as pd
 from langchain_core.prompts import PromptTemplate
-from langchain.chains import LLMChain
+# from langchain.chains import LLMChain
 from langchain_ollama import OllamaLLM
 
+df = pd.read_csv("./data/sales_data.csv", encoding="latin1")
+
+summary_text = df.describe().to_string()
+
 template = """
-    You are an experienced data analyst. Analyze the following text {text}.
+    You are an experienced data analyst. Analyze the following statistics and generate for me a smart summary that explains the changes and notes: {text}.
 """
 
 prompt = PromptTemplate.from_template(template)
@@ -14,5 +18,7 @@ llm = OllamaLLM(model="llama3")
 chain = prompt | llm
 
 # result = chain.run({"text": "The latest quarter sales has been droped down by 12%"})
-result = chain.invoke({"text": "The latest quarter sales has been droped down by 12%"})
+# result = chain.invoke({"text": "The latest quarter sales has been droped down by 12%"})
+result = chain.invoke({"text": summary_text})
+print("=== Analyzing Sales ===")
 print(result)
